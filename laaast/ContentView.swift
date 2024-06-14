e03930bb6030b610a98f458e5756e7b5541ab1b2
matthiasmeissen <meissenmatthias@gmail.com>
2024-06-14 22:29:55 +0200

    Create WelcomeView and use in ContentView

--- laaast/ContentView.swift
+++ laaast/ContentView.swift
@@ -9,22 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            ZStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 120, height: 120)
-                    .foregroundColor(.accentColor)
-                Image(systemName: "lasso.and.sparkles")
-                    .font(.system(size: 60))
-            }.padding(.bottom)
-            Text("LAAAST")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.bottom, 1.0)
-            Text("Get your excitement back on track.")
-                .foregroundColor(Color.gray)
-        }
-        .padding()
+        WelcomeView()
     }
 }
 
---
+++ laaast/WelcomeView.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeView.swift
+//  laaast
+//
+//  Created by Matthias Meissen on 14.06.24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 120, height: 120)
+                    .foregroundColor(.accentColor)
+                Image(systemName: "lasso.and.sparkles")
+                    .font(.system(size: 60))
+            }.padding(.bottom)
+            Text("LAAAST")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 1.0)
+            Text("Get your excitement back on track.")
+                .foregroundColor(Color.gray)
+        }
+        .padding()
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
